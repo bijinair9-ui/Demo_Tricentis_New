76ba1461f1f195f7ec42500f59d8f0145a9dca4c
bijinair9-ui <bijinair9@gmail.com>
2026-07-06 21:07:01 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,16 @@ import pytest
 
 from selenium import webdriver
 
-@pytest.fixture(params=['chrome','edge','firefox'])
+@pytest.fixture(params=['chrome'])
 def setup_and_teardown(request):
     parameter = request.param
 
     if parameter == 'chrome':
         driver = webdriver.Chrome()
-    if parameter == 'edge':
-        driver = webdriver.Edge()
-    if parameter == 'firefox':
-        driver = webdriver.Firefox()
+    # if parameter == 'edge':
+    #     driver = webdriver.Edge()
+    # if parameter == 'firefox':
+    #     driver = webdriver.Firefox()
 
     driver.maximize_window()
     driver.get("https://demowebshop.tricentis.com/")
